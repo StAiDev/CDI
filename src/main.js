fad0759ab3b701bd69bd92def79532fad0d64bd4
shinyangdevelop <shinyangdevelop@gmail.com>
2022-01-18 01:01:27 +0900

    Modify init size

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ let mainWindow = null
 
 function createWindow() {
     mainWindow = new BrowserWindow( {
-        width: 800,
-        height: 600,
+        width: 1920,
+        height: 1080,
         webPreferences: {
             nodeIntegration: true
         }
