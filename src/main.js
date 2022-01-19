698b8a850146f7b82f80bb8034c1dd20ed299608
shinyangdevelop <shinyangdevelop@gmail.com>
2022-01-19 18:32:31 +0900

    Add custom menubar to the application, and change loadFile(line 40) to loadURL

--- src/main.js
+++ src/main.js
@@ -1,18 +1,55 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu} = require('electron')
+const path = require('path')
+
+const template = [
+    {
+        label: "File",
+        submenu: [
+            {role: 'close'},
+            {role: 'quit'}
+        ]
+    },
+    {
+        label: "Tools",
+        submenu: [
+            {
+                label: "Test",
+                click() {
+                    console.log("Test!")
+                }
+            }
+        ]
+    }
+]
 
 let mainWindow = null
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
-function createWindow() {
-    mainWindow = new BrowserWindow( {
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
         width: 1920,
-        height: 1080,
+        height:1080,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        title: "CDI Main Window",
+        icon: path.join(`${__dirname}/../res/icon/cdi_revert.png`)
     })
-
-    mainWindow.setMenuBarVisibility(false)
-    mainWindow.loadFile('src/windows/mainWindow.html')
+    
+    mainWindow.loadURL('https://learning.chungdahm.com')
 }
 
-app.on('ready', createWindow)
+app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', ()=> {
+    if(win === null) {
+        createMainWindow()
+    }
+})
